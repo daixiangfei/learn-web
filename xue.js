e458262c621d4f6dd3377641e322719ae803c067
daixiangfei <daixiangfei0605@163.com>
2019-01-03 00:01:43 +0800

    js

---
+++ xue.js
@@ -0,0 +1,15 @@
+console.log("这是一个js文件")
+var name = "代翔飞"
+var nameList = ['dxf','dxw','dyy']
+console.log(nameList.length)
+console.log(nameList[1],nameList[2])
+if(nameList[0]=="dxf"){
+    console.log("代翔飞")
+}
+for(var i = 0;i<nameList.length;i++){
+    console.log(nameList[i])
+}
+var o=0
+while(o<101){
+    console.log(o++)
+}
